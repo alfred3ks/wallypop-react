c06cff7729236dba2667375ee4ddcbc4c4149545
Alfredo Sánchez <alfred3ks@gmail.com>
2023-12-04 19:08:58 +0100

    Creo las rutas que tendra la app.

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Routes, Route } from 'react-router';
+import { Routes, Route, Navigate } from 'react-router';
 import LoginPage from './pages/auth/LoginPage';
 import Signup from './pages/signup/SignupPage';
 
@@ -9,6 +9,19 @@ const App = () => {
     <Routes>
       <Route path="/login" element={<LoginPage />} />
       <Route path="/signup" element={<Signup />} />
+      {/* Rutas anidades */}
+      <Route path="/adverts" element={<div>Layout</div>}>
+        <Route index element={<div>Adverts</div>} />
+        <Route path=":id" element={<div>Ruta dinamica adverts id</div>} />
+        <Route path="new" element={<div>Nuevo anuncio</div>} />
+      </Route>
+
+      {/* Redireccion */}
+      <Route path="/" element={<Navigate to={'/adverts'} />} />
+      {/* pagina 404 */}
+      <Route path="/404" element={<div>404 | Not Fount</div>} />
+      {/* pagina 404 */}
+      <Route path="*" element={<Navigate to={'/404'} />} />
     </Routes>
   );
 };
