0027a15de9ca54496bd246ad9f72647fd4da3ba3
Alfredo Sánchez <alfred3ks@gmail.com>
2023-12-05 20:47:00 +0100

    Creo el formulario para crear un nuevo anuncio.

--- src/pages/adverts/NewAdvertPage/index.jsx
+++ src/pages/adverts/NewAdvertPage/index.jsx
@@ -1,5 +1,114 @@
+import { useState } from 'react';
+
+// const capitalizeFirstLetter = (input) => {
+//   return input.charAt(0).toUpperCase() + input.slice(1);
+// };
+
+// const Input = ({ label, id, type, value, onChange }) => {
+//   const handleInputChange = (e) => {
+//     const inputValue = e.target.value;
+//     const capitalizedValue = capitalizeFirstLetter(inputValue);
+//     onChange(capitalizedValue);
+//   };
+
+//   return (
+//     <div>
+//       <label htmlFor={id}>{label}</label>
+//       <br />
+//       <input type={type} id={id} value={value} onChange={handleInputChange} />
+//     </div>
+//   );
+// };
+
 const NewAdvertPage = () => {
-  return <h1>NewAdvertPage</h1>;
+  const [advert, setAdvert] = useState({
+    name: '',
+    sale: false,
+    tags: [],
+    price: '',
+    photo: '',
+  });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(advert);
+  };
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setAdvert((currentAdvert) => {
+      return {
+        ...currentAdvert,
+        [name]:
+          name === 'tags'
+            ? value.split(',')
+            : name === 'price'
+            ? parseFloat(value)
+            : value,
+      };
+    });
+  };
+
+  return (
+    <div>
+      <h2>Producto</h2>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="name">Nombre del producto:</label>
+        <br />
+        <input
+          type="text"
+          id="name"
+          name="name"
+          value={advert.name}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="type">Tipo de producto:</label>
+        <br />
+        <select
+          id="type"
+          name="type"
+          value={advert.sale ? 'venta' : 'compra'}
+          onChange={handleChange}
+        >
+          <option value="compra">Compra</option>
+          <option value="venta">Venta</option>
+        </select>
+        <br />
+
+        <label htmlFor="tags">Introduce los Tags separados por comas:</label>
+        <br />
+        <input
+          type="text"
+          id="tags"
+          name="tags"
+          value={advert.tags}
+          onChange={handleChange}
+        />
+
+        <br />
+        <label htmlFor="price">Precio:</label>
+        <br />
+        <input
+          type="text"
+          id="price"
+          name="price"
+          value={advert.price}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="photo">Foto del producto:</label>
+        <br />
+        <input type="file" id="photo" name="photo" onChange={handleChange} />
+        <br />
+
+        <button type="submit">Enviar</button>
+      </form>
+    </div>
+  );
 };
 
 export default NewAdvertPage;
