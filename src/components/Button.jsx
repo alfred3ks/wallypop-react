8cb7b495955829ea5e0806e6ce21ec571a0da820
Alfredo Sánchez <alfred3ks@gmail.com>
2023-12-08 18:32:54 +0100

    Cambio los estilos al componente Button.

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -2,9 +2,8 @@ import styled from 'styled-components';
 
 const Button = styled.button`
   padding: 6px 10px;
-  font-size: 1.3rem;
-  /* text-align: center; */
-  background-color: #0e4564;
+  /* font-size: 1.2rem; */
+  background-color: #252a3f;
   color: ${(props) => (props.$variant === 'primary' ? '#fff' : '#fff')};
   opacity: ${(props) => (props.disabled ? 0.5 : 1)};
   pointer-events: ${(props) => (props.disabled ? 'none' : 'auto')};
