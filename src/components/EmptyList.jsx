9038607c6daf64c9bead289e80a7edd350594d69
Alfredo Sánchez <alfred3ks@gmail.com>
2023-12-09 17:23:00 +0100

    Agrego lo estilos al componente EmptyList.

--- src/components/EmptyList.jsx
+++ src/components/EmptyList.jsx
@@ -1,16 +1,29 @@
+import styled from 'styled-components';
 import Button from './Button';
 import { Link } from 'react-router-dom';
 
 // eslint-disable-next-line react/prop-types
 const EmptyList = ({ message }) => {
   return (
-    <div>
-      <p>{message}.</p>
+    <Container>
+      <Text>{message}</Text>
       <Button as={Link} to={'/login'} $variant={'primary'}>
         Iniciar sesión
       </Button>
-    </div>
+    </Container>
   );
 };
+
+const Container = styled.div`
+  display: grid;
+  align-items: center;
+  justify-content: center;
+  place-items: center;
+`;
+
+const Text = styled.p`
+  margin: 10px 0;
+  font-size: 1.6rem;
+`;
 
 export default EmptyList;
