a9e2e165a53290b2fa4e0380c550a8c7718dd662
Alfredo Sánchez <alfred3ks@gmail.com>
2023-12-09 17:47:47 +0100

    Creo los estilos del formulario de crear anuncio.

--- src/pages/adverts/NewAdvertPage/index.jsx
+++ src/pages/adverts/NewAdvertPage/index.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import { createAdvert } from '../service';
 import Button from '../../../components/Button';
 import { useNavigate } from 'react-router';
+import styled from 'styled-components';
 
 const NewAdvertPage = () => {
   const [isFeching, setIsFeching] = useState(false);
@@ -70,68 +71,111 @@ const NewAdvertPage = () => {
   const buttonDisabled = !(name && sale && tags && price) || isFeching;
 
   return (
-    <div>
-      <h2>Crear un nuevo Producto</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor="name">Nombre del producto:</label>
-          <input
-            type="text"
-            id="name"
-            name="name"
-            value={formData.name}
-            onChange={handleChange}
-          />
-        </div>
+    <Container>
+      <Title>Crear un Anuncio</Title>
+      <Form onSubmit={handleSubmit}>
+        <Label htmlFor="name">Nombre del producto:</Label>
+        <Input
+          type="text"
+          id="name"
+          name="name"
+          value={formData.name}
+          onChange={handleChange}
+        />
 
-        <div>
-          <label htmlFor="type">Tipo de producto:</label>
-          <select name="sale" value={formData.sale} onChange={handleChange}>
-            <option value={false}>Venta</option>
-            <option value={true}>Compra</option>
-          </select>
-        </div>
+        <Label htmlFor="type">Tipo de producto:</Label>
+        <Select name="sale" value={formData.sale} onChange={handleChange}>
+          <option value={false}>Venta</option>
+          <option value={true}>Compra</option>
+        </Select>
 
-        <div>
-          <label htmlFor="tags">Tags separados por comas:</label>
-          <input
-            type="text"
-            id="tags"
-            name="tags"
-            value={formData.tags}
-            onChange={handleChange}
-          />
-        </div>
+        <Label htmlFor="tags">Tags separados por comas:</Label>
+        <Input
+          type="text"
+          id="tags"
+          name="tags"
+          value={formData.tags}
+          onChange={handleChange}
+        />
 
-        <div>
-          <label htmlFor="price"> Precio:</label>
-          <input
-            type="text"
-            id="price"
-            name="price"
-            value={formData.price}
-            onChange={handleChange}
-          />
-        </div>
+        <Label htmlFor="price"> Precio:</Label>
+        <Input
+          type="text"
+          id="price"
+          name="price"
+          value={formData.price}
+          onChange={handleChange}
+        />
 
-        <div>
-          <label htmlFor="photo">Foto del producto:</label>
-          <input
-            type="file"
-            id="photo"
-            name="photo"
-            accept="image/*"
-            onChange={handleChange}
-          />
-        </div>
+        <Label htmlFor="photo">Foto del producto:</Label>
+        <Input
+          type="file"
+          id="photo"
+          name="photo"
+          accept="image/*"
+          onChange={handleChange}
+        />
 
         <Button $variant={'primary'} type="submit" disabled={buttonDisabled}>
           {!isFeching ? 'Enviar' : 'Enviando...'}
         </Button>
-      </form>
-      {error && <div>{error.message}</div>}
-    </div>
+      </Form>
+      {error && <Message>{error.message}</Message>}
+    </Container>
   );
 };
+
+const Container = styled.div`
+  width: 300px;
+  padding: 20px;
+  margin: auto;
+  margin-top: 30px;
+  background-color: #ffffff;
+  border-radius: 5px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+
+  @media screen and (width >= 768px) {
+    width: 400px;
+  }
+`;
+
+const Title = styled.h2`
+  font-size: 2.4rem;
+  text-align: center;
+  color: #333;
+  margin-bottom: 10px;
+`;
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const Label = styled.label`
+  font-weight: bold;
+  color: #333;
+`;
+
+const Input = styled.input`
+  width: 100%;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+
+const Select = styled.select`
+  width: 100%;
+  margin-top: 5px;
+  padding: 8px;
+  border: 1px solid #ccc;
+  border-radius: 3px;
+`;
+
+const Message = styled.div`
+  color: #ff0000;
+  text-align: center;
+  margin-top: 10px;
+`;
 
 export default NewAdvertPage;
